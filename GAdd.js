086ac33087cf6e5b6d3c3ed2b755a601b1abd1fd
vAAAmsi <103809854+vAAAmsi@users.noreply.github.com>
2022-09-02 18:10:40 +0530

    Add files via upload

---
+++ GAdd.js
@@ -0,0 +1,54 @@
+function ad()
+{
+    var a=document.getElementById("bv")
+    a.style.display="none"
+    var x=document.getElementById("90")
+    x.style.display="none"
+    var y=document.getElementById("10")
+  y.style.display="block"
+  var p=document.getElementById("30")
+  p.style.display="block"
+ 
+    
+//   var c=document.getElementById("1")
+//     c.style.display="block"
+    
+}
+ function sf()
+ {
+    var y=document.getElementById("10")
+    y.style.display="none"
+    var c=document.getElementById("1")
+     c.style.display="block"
+    
+     }    
+
+function vam()
+  {
+            var a=document.getElementById("4")
+            a.style.display="block"
+            var c=document.getElementById("10")
+     c.style.display="none"
+  }
+     
+ function GOPINI()
+ {
+    var c=document.getElementById("1")
+    c.style.display="none"
+     var g=document.getElementById("2")
+     g.style.display="block"
+     
+   
+    setTimeout(f, 2000);
+
+function f() {
+   
+  
+  
+    var da=document.getElementById("6")
+    da.style.display="block"
+    var g=document.getElementById("2")
+      g.style.display="none"
+  
+}
+ }
